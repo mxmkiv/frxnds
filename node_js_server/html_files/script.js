3f213dd8e28f545787d9cdcc37bbaaa51d437e06
mxmkiv <57045650+mxmkiv@users.noreply.github.com>
2020-09-20 18:03:49 +0300

    new server

---
+++ node_js_server/html_files/script.js
@@ -0,0 +1,9 @@
+//alert("JavaScript linked")
+const mysql = require("mysql2");
+
+function getInf() {
+    const email = document.getElementById("email").value;
+    alert(document.getElementById("email").value);
+}
+
+//alert(document.getElementById("email").value);
--- node_js_server/index.js
+++ node_js_server/index.js
@@ -1,37 +1,36 @@
-const express = require('express');
-const bodyParser = require("body-parser");
+const express = require("express");
 const mysql = require("mysql2");
+
 const app = express();
 
-app.use(express.static('html_files'))
+app.use(express.static('html_files'));
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "frx",
-  password: "frx24"
+    host: "localhost",
+    user: "root",
+    database: "frxnds",
+    password: "frx24"
 });
 
 connection.connect(function(err){
     if (err) {
-      return console.error("Ошибка: " + err.message);
+        return console.log('Ошибка' + err.message)
+    } else {
+        console.log('Подключение к БД установленно')
     }
-    else{
-      console.log("Подключение к базе успешно установлено");
-    }
- });
-
-let query="SELECT * FROM user";
-
-connection.query(query, (err, result) => {
-  console.log(err);
-  console.log(result);
 });
 
- app.get('/', function(req, res) {
+let queryString = 'SELECT * FROM user_info';
+
+connection.query(queryString, (err, result) => {
+    console.log(err);
+    console.log(result);
+});
+
+app.get('/', function(req, res) {
    res.sendFile(__dirname + '/html_files/index.html');
- });
+});
 
 app.listen(80, () => {
-  console.log('Сервер запущен', new Date())
+    console.log('Сервер запущен')
 });
